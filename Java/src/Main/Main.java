983eac4dc29b791526b0785eff78e504046cf954
be1zi <konrad.belzowski@gmail.com>
2017-06-11 15:16:59 +0200

    utworzenie pliku na raport

--- Java/src/Main/Main.java
+++ Java/src/Main/Main.java
@@ -1,5 +1,7 @@
 package Main;
 
+import RAID.Errors;
+
 /**
  * Created by Złoty on 2017-06-11.
  */
@@ -7,6 +9,7 @@ public class Main {
 
     public static void main(String [ ] args){
 
-        System.out.println("asd");
+        Errors err = new Errors();
+
     }
 }
--- Java/src/RAID/Errors.java
+++ Java/src/RAID/Errors.java
@@ -1,7 +1,51 @@
 package RAID;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
- * Created by Kamil on 2017-06-11.
+ * Created by Belzi on 2017-06-11.
  */
 public class Errors {
+
+   public Errors(){
+
+       textFiles();
+    }
+
+    private void textFiles() {
+
+        //pobranie  nazwy użytkownika z systemu
+        String username = System.getProperty("user.name");
+
+        //scieżki dostępu
+        String pathFolder="C:/Users/"+username+"/Documents/RAID";
+        String pathFile=pathFolder+"/Raport.txt";
+
+        //utworzenie katalogu w moich dokumentach
+        if(!Files.exists(Paths.get(pathFolder))){
+            try {
+                Files.createDirectory(Paths.get(pathFolder));
+                System.out.println("Utworzono katalog");
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        //utworzenie pliku w moich dokumentach
+        if(!Files.exists(Paths.get(pathFile))){
+            try {
+                Files.createFile(Paths.get(pathFile));
+                System.out.println("Utoworzono plik z raportem");
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+    public void genetateRaport(){
+
+    }
 }
